5b6933456fb8490363d47c4d6c7959af3e557778
Yofou <exodias1965@gmail.com>
2022-05-07 04:20:22 +0100

    [📦] Encapsulated Three Canvas logic

--- src/component/3dcomponent.tsx
+++ src/component/3dcomponent.tsx
@@ -1,47 +1,35 @@
 import React from 'react';
-import ViewGL from './ViewGL';
+import SceneManager from '../three/main';
 
 export default class Scene extends React.Component {
+	canvasRef: React.RefObject<HTMLCanvasElement>
+	viewGL: SceneManager
+	onResize: () => void
+	
     constructor(props) {
         super(props);
         this.canvasRef = React.createRef();
+		
+		this.onResize = () => this.viewGL.resizeCanvas()
     }
 
     // ******************* COMPONENT LIFECYCLE ******************* //
     componentDidMount() {
         // Get canvas, pass to custom class
         const canvas = this.canvasRef.current;
-        this.viewGL = new ViewGL(canvas);
+        this.viewGL = new SceneManager(canvas);
 
-        // Init any event listeners
-        window.addEventListener('mousemove', this.mouseMove);
-        window.addEventListener('resize', this.handleResize);
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        // Pass updated props to 
-        const newValue = this.props.whateverProperty;
-        this.viewGL.updateValue(newValue);
+        window.addEventListener('resize', this.onResize);
     }
 
     componentWillUnmount() {
         // Remove any event listeners
-        window.removeEventListener('mousemove', this.mouseMove);
-        window.removeEventListener('resize', this.handleResize);
+        window.removeEventListener('resize', this.onResize);
     }
-
-    // ******************* EVENT LISTENERS ******************* //
-    mouseMove = (event) => {
-        this.viewGL.onMouseMove();
-    }
-
-    handleResize = () => {
-        this.viewGL.onWindowResize(window.innerWidth, window.innerHeight);
-    };
 
     render() {
         return (
-            <div class="canvasContainer">
+            <div className="canvasContainer">
                 <canvas ref={this.canvasRef} />
             </div>
         );
--- src/p5/p5mapper.tsx
+++ src/p5/p5mapper.tsx
@@ -1,84 +1,88 @@
-import { Component } from 'react'
-import Sketch from 'react-p5'
-import P5 from "p5";
-import { Ratio } from 'react-bootstrap';
-import { Boundary } from '../func/Boundary'
-import { pointer } from '../func/Select'
-
+import { Component } from "react";
+import Sketch from "react-p5";
+import type P5 from "p5/index"
+import { Ratio } from "react-bootstrap";
+import { Boundary } from "./func/Boundary";
+import { pointer } from "./func/Select";
 
 class P5JS_App extends Component {
+	x: number
+	y: number
+	cw: number
+	ch: number
+	walls: Boundary[]
+	particle: any[]
+	curser: number
+	nodes: any[]
+	createMode: boolean
+	sw: number
+	sh: number
 
-    constructor() {
-        super();
-        p5 = P5;
-        x = 100;
-        y = 100;
-        cw = 800;
-        ch = 600;
-        walls = []
-        particle = []
-        curser = 0;
-        nodes = []
-        createMode = false;
-        sw = this.cw;  //sketch width
-        sh = this.ch;  //sketch height
-        curser = null; 
-    };
+	constructor(props) {
+		super(props);
+		this.x = 100;
+		this.y = 100;
+		this.cw = 800;
+		this.ch = 600;
+		this.walls = [];
+		this.particle = [];
+		this.curser = 0;
+		this.nodes = [];
+		this.createMode = false;
+		this.sw = this.cw; //sketch width
+		this.sh = this.ch; //sketch height
+	}
 
+	setup(p5: any, parent: Element) {
+		p5.createCanvas(this.cw, this.ch).parent(parent);
+		p5.colorMode(p5.HSB, 360, 100, 100);
+		p5.ellipseMode(p5.RADIUS);
 
-    setup = (p5, parent) => {
+		//this.walls.push(new Boundary(p5, 0 , 0 , this.sw, 0))
+		//this.walls.push(new Boundary(p5, this.sw, 0 , this.sw, this.sh))
+		//this.walls.push(new Boundary(p5, this.sw, this.sh, 0 , this.sh))
+		//this.walls.push(new Boundary(p5, 0 , this.sh, 0 , 0))
 
-        this.parent = parent;
-        p5.createCanvas(this.cw, this.ch).parent(parent);
+		this.curser = new pointer();
+	};
 
-        p5.colorMode(p5.HSB, 360, 100, 100);
-        p5.ellipseMode(p5.RADIUS);
+	drawGrid(p5: P5, sizX: number, sizY: number, spcX: number, spcY: number) {
+		//  console.log(p5);
+		p5.stroke(55);
+		p5.strokeWeight(1);
+		for (var x = 0; x < sizX; x += spcX) {
+			for (var y = 0; y < sizY; y += spcY) {
+				p5.line(x, 0, x, sizY);
+				p5.line(0, y, sizX, y);
+			}
+		}
+	};
 
-        //this.walls.push(new Boundary(p5, 0 , 0 , this.sw, 0))
-        //this.walls.push(new Boundary(p5, this.sw, 0 , this.sw, this.sh))
-        //this.walls.push(new Boundary(p5, this.sw, this.sh, 0 , this.sh))
-        //this.walls.push(new Boundary(p5, 0 , this.sh, 0 , 0))
+	draw(p5: any) {
+		p5.clear(0, 0, 0, 0);
+		p5.background(0);
+		p5.fill(255);
+		p5.translate(50, -50);
+		this.drawGrid(p5, 600, 600, 100, 100);
+	};
 
-        this.curser = new pointer()
-
-
-    }
-
-    drawGrid = (p5, sizX, sizY, spcX, spcY) => {
-      //  console.log(p5);
-        p5.stroke(55);
-        p5.strokeWeight(1);
-        for (var x = 0; x < sizX; x += spcX) {
-            for (var y = 0; y < sizY; y += spcY) {
-                p5.line(x, 0, x, sizY);
-                p5.line(0, y, sizX, y);
-            }
-        }
-    }
-
-
-    draw = (p5) => {
-        p5.clear();
-        p5.background(0);
-        p5.fill(255);
-        //p5.rect(this.x, this.y, 50, 50);
-        p5.translate(50,-50);
-
-        this.drawGrid(p5, 600,600,100,100);
-
-
-
-    }
-
-    render() {
-
-        return (
-            <div>
-                <Ratio aspectRatio="4x3" className="border border-5 rounded rounded-3">
-                <Sketch setup={this.setup} draw={this.draw} className="w-100 h-100" />
-                </Ratio>;
-            </div>
-            )
-    }
+	render() {
+		return (
+			<div>
+				<Ratio
+					aspectRatio="4x3"
+					className="border border-5 rounded rounded-3"
+				>
+					<Sketch
+						setup={(p5, parent) => this.setup(p5, parent)}
+						draw={(p5) => this.draw(p5)}
+						className="w-100 h-100"
+					/>
+				</Ratio>
+				;
+			</div>
+		);
+	}
 }
-export default P5JS_App
+
+export default P5JS_App;
