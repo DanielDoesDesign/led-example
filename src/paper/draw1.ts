608c6131203acdb7a262c7e61b7409fcb616fc5e
DanielDoesDesign <designdoesdaniel@gmail.com>
2022-05-12 19:19:53 +1200

    hhjl

--- src/paper/draw1.ts
+++ src/paper/draw1.ts
@@ -1,8 +1,30 @@
 import Paper from "paper";
+import { loadedData } from '../three/core/helperFunc'
 
 const draw1 = () => {
 
-  
+//Set background
+var from = new Paper.Point(0, 0);
+var to = new Paper.Point(Paper.view.viewSize.width, Paper.view.viewSize.height);
+var rect = new Paper.Path.Rectangle(from, to);
+rect.strokeColor = new Paper.Color('red')
+rect.fillColor = new Paper.Color('white')
+
+let lines = []
+
+
+var data = loadedData();
+lines = data.outLines;
+
+for (let i = 0; i < lines.length; i++) {
+   var newLine = new Paper.Path.Line([lines[i].x, lines[i].y]);
+   newLine.strokeColor = new Paper.Color('black')
+   lineTo(point)
+}
+
+
+
+
 var star = new Paper.Path.Star({
     center: Paper.view.center,
     points: 6,
@@ -17,20 +39,20 @@ var circle = new Paper.Path.Circle({
     strokeColor: 'black'
 });
 
-var bg = new Paper.Path.Rectangle({
-  center: Paper.view.center,
-  radius: 500,
-  strokeColor: 'red'
-});
+
+
+
+var bg = new Paper.Path.Rectangle(rect);
+
 
 // Create a group of the two items and clip it:
 var group = new Paper.Group(circle, star);
 group.clipped = true;
 
-var group2 = new Paper.Group(circle2)
-
 //onFrame(event)
 Paper.view.onFrame = onFrame;
+Paper.view.viewSize.width = 600;
+Paper.view.viewSize.height = 400;
 
 // Lets animate the circle:
 function onFrame(event) {
@@ -41,6 +63,7 @@ function onFrame(event) {
 
 
 };
+
 
 
 export default draw1;
